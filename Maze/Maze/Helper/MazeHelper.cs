ac871133f3cf747354c0736eecfbda0e5772b051
James Lewel Padecio <jameslewel.padecio@cit.edu>
2022-11-26 21:15:52 +0800

    Add generate point method in maze helper class

--- Maze/Maze/Helper/MazeHelper.cs
+++ Maze/Maze/Helper/MazeHelper.cs
@@ -1,16 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Drawing;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Maze.Helper
 {
     public class MazeHelper
     {
+        Point point;
+
+        int row;
+        int column;
+
         [DllImport("user32.dll")]
         public static extern int SendMessage(IntPtr hWnd, Int32 wMsg, bool wParam, Int32 lParam);
 
@@ -29,6 +30,22 @@ namespace Maze.Helper
                 SendMessage(control.Handle, WM_SETREDRAW, true, 0);
                 control.Refresh();
             }
+        }
+
+        public Point GeneratePoint(int maxRow, int maxColumn)
+        {
+            Random random = new Random((DateTime.Now.Millisecond + DateTime.Now.Second) * 
+                                       (DateTime.Now.Millisecond - DateTime.Now.Second));
+            
+            // Generates random number in range
+            row = random.Next(0, maxRow);
+            column = random.Next(0, maxColumn);
+
+            // Creates new point
+            point.X = row;
+            point.Y = column;
+
+            return point;
         }
     }
 }
